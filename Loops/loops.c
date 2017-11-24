c516119fdad75796b634dd81e1995eaed9ad7562
Alex Ryan <alsryan@rams.colostate.edu>
2017-11-24 09:33:43 -0700

    Made changes to Loops project

--- Loops/loops.c
+++ Loops/loops.c
@@ -1,2 +1,15 @@
 #include  <stdio.h>
 
+int square(int sideLen){
+	char spacer[5] = "     ";
+	int count = sideLen;
+	printf("%d",sideLen);
+	while(count > 0){
+		printf("%c",'*');
+		count--;
+	}
+	printf("\n");
+	count = sideLen;
+
+	return 0;
+}
--- Loops/main.c
+++ Loops/main.c
@@ -1,7 +1,14 @@
 #include  <stdio.h>
+#include <time.h>
+
 
 int main(int argc, char *argv[]){	
 	printf("The loops are functional!");
 	printf("\n");
+	printf("\n");
+	time_t seconds;
+
+   seconds = time(NULL);
+   printf("Seconds since January 1, 1970 = %ld\n", seconds);
 	return 0;
 }
